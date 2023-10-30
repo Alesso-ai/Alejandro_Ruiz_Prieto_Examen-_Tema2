346398c33c9b64b34c6e08a04ce2b5676e3d8cfe
Alejandro Ruiz <alejandroruizprieto@alumnos.tuniversformacion.es>
2023-10-30 09:21:13 +0100

    Montando a estructura de JS

--- scripts/main.js
+++ scripts/main.js
@@ -3,3 +3,12 @@
 
     Git Hub :
 */ 
+
+
+window.addEventListener("load", window);
+
+const btnDepositar = document.getElementById("btnDepositar");
+const btnRetirar = document.getElementById("btnRetirar");
+const btnTransferir = document.getElementById("btnTransferir");
+const saldoTexto = document.getElementById("saldoTexto");
+const btnSalir = document.getElementById("btnSalir");
