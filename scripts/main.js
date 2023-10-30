44249ade22bca11d34df7df6a0a1dd014a2fc081
Alejandro Ruiz <alejandroruizprieto@alumnos.tuniversformacion.es>
2023-10-30 09:35:55 +0100

    Creando las funciones de JS

--- scripts/main.js
+++ scripts/main.js
@@ -5,10 +5,51 @@
 */ 
 
 
-window.addEventListener("load", window);
+window.addEventListener("loaded", window);
 
+
+/*Creamos 3 variables 1 constante para el PIN y otras
+2 para el saldo que se ira modificando y otra para los intentos*/
+
+const PIN = "1234";
+let intentos = 3;
+let saldo = 1000;
+
+
+
+/*Creamos las constantes de los botones para pasarlos despues por id al html*/ 
 const btnDepositar = document.getElementById("btnDepositar");
 const btnRetirar = document.getElementById("btnRetirar");
 const btnTransferir = document.getElementById("btnTransferir");
 const saldoTexto = document.getElementById("saldoTexto");
 const btnSalir = document.getElementById("btnSalir");
+const btnContraseña = document.getElementById("btnContraseña");
+
+function mostrarSaldo(){
+    const saldo = document.getElementById("saldo")
+
+
+
+}
+
+
+function depositar(){
+
+}
+
+function retirar(){
+
+}
+
+function transferir(){
+
+}
+
+
+function  cambioContraseña(){
+
+}
+
+
+
+
